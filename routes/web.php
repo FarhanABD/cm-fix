cbad3f04b21b31dcea5d46f02c69e054a3d661dd
Najwa04 <najwa.am2003@gmail.com>
2024-08-19 15:04:46 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+Route::get('super-admin/login', [SuperAdminController::class, 'login'])->name('super-admin.login');
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
